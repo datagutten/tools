135a9c8d36eaf51a35f69b27b90987c8cb850825
datagutten <datagutten@datagutten.net>
2013-06-08 11:09:08 +0200

    Wrapped the command check in a class

--- dependcheck.php
+++ dependcheck.php
@@ -1,22 +1,31 @@
 <?Php
 //Check if a shell command exists
-function depend($check)
+class dependcheck
 {
-	if(PHP_OS=='WINNT')
-		$checkcommand='where';
-	else
-		$checkcommand='which';
-	if(!is_array($check))
-		$check=array($check);
-	$notfound=array();
-	foreach ($check as $command)
-	{		
-		if(shell_exec("$checkcommand $command 2>NUL")=='') //stderr is redirected to NUL, so if a string is returned the command exists
-			$notfound[]=$command; //Add it to the lists of commands not found
+	private $checkcommand;
+	function __construct()
+	{
+		if(PHP_OS=='WINNT')
+			$this->checkcommand='where';
+		else
+			$this->checkcommand='which';
 	}
-	if(count($notfound)>0)
-		return $notfound;
-	else
-		return true;
+	function depend($check)
+	{
+		$check=(array)$check;
+		$notfound=array();
+		foreach ($check as $command)
+		{		
+			if(shell_exec($this->checkcommand." $command 2>NUL")=='') //stderr is redirected to NUL, so if a string is returned the command exists
+				$notfound[]=$command; //Add it to the lists of commands not found
+		}
+		if(count($notfound)>0)
+			return $notfound;
+		else
+			return true;
+	}
 }
+/*Sample:
+$dependcheck=new dependcheck;
+var_dump($dependcheck->depend('mediainfo'));*/
 ?>
