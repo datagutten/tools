e5f1b663c5b394f932072b8ef9e5c20228697d38
datagutten <datagutten@datagutten.net>
2013-06-09 18:17:23 +0200

    null is changed depending of OS

--- dependcheck.php
+++ dependcheck.php
@@ -3,12 +3,19 @@
 class dependcheck
 {
 	private $checkcommand;
+	private $null;
 	function __construct()
 	{
 		if(PHP_OS=='WINNT')
+		{
 			$this->checkcommand='where';
+			$this->null='NUL';
+		}
 		else
+		{
 			$this->checkcommand='which';
+			$this->null='/dev/null';	
+		}
 	}
 	function depend($check)
 	{
@@ -16,7 +23,7 @@ class dependcheck
 		$notfound=array();
 		foreach ($check as $command)
 		{		
-			if(shell_exec($this->checkcommand." $command 2>NUL")=='') //stderr is redirected to NUL, so if a string is returned the command exists
+			if(shell_exec($this->checkcommand." $command 2>".$this->null)=='') //stderr is redirected to NUL, so if a string is returned the command exists
 				$notfound[]=$command; //Add it to the lists of commands not found
 		}
 		if(count($notfound)>0)
