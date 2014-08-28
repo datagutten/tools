8363c18feaaea12732a92b64b151a8fda39b052f
datagutten <datagutten@datagutten.net>
2014-08-28 19:25:09 +0200

    Added tool to remove characters not valid in windows file names

---
+++ filnavn.php
@@ -0,0 +1,10 @@
+<?Php
+function filnavn($tittel)
+{
+	$filnavn=html_entity_decode($tittel);
+	$filnavn=str_replace(array(':','?','*','|','<','>','/','\\'),array('-','','','','','','',''),$filnavn); //Fjern tegn som ikke kan brukes i filnavn på windows
+	if(PHP_OS=='WINNT')
+		$filnavn=utf8_decode($filnavn);
+	return $filnavn;
+}
+?>
