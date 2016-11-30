8fe1603ca59ca75dbbc37e33ced779109bbbba32
datagutten <datagutten@datagutten.net>
2016-11-30 19:13:05 +0100

    Improved character replacements in file names

--- filnavn.php
+++ filnavn.php
@@ -2,7 +2,8 @@
 function filnavn($tittel)
 {
 	$filnavn=html_entity_decode($tittel);
-	$filnavn=str_replace(array(':','?','*','|','<','>','/','\\'),array('-','','','','','','',''),$filnavn); //Fjern tegn som ikke kan brukes i filnavn på windows
+	$filnavn=str_replace(array(':','?','*','|','<','>','/','\\'),array(' -','','','','','','-','-'),$filnavn); //Fjern tegn som ikke kan brukes i filnavn på windows
+
 	if(PHP_OS=='WINNT')
 		$filnavn=utf8_decode($filnavn);
 	return $filnavn;
