87f18261db520f6a7570bd7591350f0e7c43e0f2
datagutten <datagutten@datagutten.net>
2019-10-27 16:48:36 +0100

    Add XML parsing for <position> tag

--- color/xml.php
+++ color/xml.php
@@ -4,6 +4,7 @@
 namespace datagutten\tools\color;
 
 
+use InvalidArgumentException;
 use SimpleXMLElement;
 
 class xml
@@ -20,5 +21,22 @@ class xml
         $color['low'] = intval($color['low']);
         $color['high'] = intval($color['high']);
         return $color;
+    }
+
+    /**
+     * Parse a <position> element
+     * @param SimpleXMLElement $position_xml
+     * @return array
+     */
+    public static function parse_position($position_xml)
+    {
+        $tag = $position_xml->getName();
+        if($tag!=='position')
+            throw new InvalidArgumentException('Invalid tag: '.$tag);
+        $attributes = $position_xml->attributes();
+        $position = ['x'=>(int)$attributes['x'], 'y'=>(int)$attributes['y']];
+        if(!empty($position_xml->{'color'}))
+            $position['color'] = self::parse_color($position_xml->{'color'});
+        return $position;
     }
 }
--- tests/color/xmlTest.php
+++ tests/color/xmlTest.php
@@ -14,4 +14,36 @@ class xmlTest extends TestCase
         $color_ref = ['reference'=>0xfdfdfd, 'low'=>-5, 'high'=>5];
         $this->assertSame($color_ref, $color);
     }
+
+    public function testParsePosition()
+    {
+        $xml = simplexml_load_file(__DIR__.'/test_data/color_position.xml');
+        $color = xml::parse_color($xml->{'position'}->{'color'});
+        $position_ref = ['x'=>500, 'y'=>600, 'color'=>$color];
+        $position = xml::parse_position($xml->{'position'});
+        $this->assertSame($position_ref, $position);
+    }
+
+    public function testParsePositionNoColor()
+    {
+        $xml = simplexml_load_file(__DIR__.'/test_data/position_common_color.xml');
+        $position = $xml->xpath('/positions/position[2]')[0];
+        $position_ref = ['x'=>500, 'y'=>600];
+        $position = xml::parse_position($position);
+        $this->assertSame($position_ref, $position);
+    }
+
+    public function testPositionInvalidArgument()
+    {
+        $xml = new SimpleXMLElement('<color></color>');
+        $this->expectException(InvalidArgumentException::class);
+        xml::parse_position($xml);
+    }
+
+    public function testColorInvalidArgument()
+    {
+        $xml = new SimpleXMLElement('<position></position>');
+        $this->expectException(InvalidArgumentException::class);
+        xml::parse_color($xml);
+    }
 }
