de46fad582ad7226ebf0832c002acec645e37488
Anders Birkenes <datagutten@datagutten.net>
2026-03-22 11:26:07 +0100

    Use mb_convert_encoding to convert UTF8 to ASCII

--- filnavn.php
+++ filnavn.php
@@ -5,6 +5,14 @@ function filnavn($tittel)
 	$filnavn=str_replace(array(':','?','*','|','<','>','/','\\','"'),array(' -','','','','','','-','-',''),$filnavn); //Fjern tegn som ikke kan brukes i filnavn på windows
 
 	if(PHP_OS=='WINNT')
-		$filnavn=utf8_decode($filnavn);
+    {
+        if (function_exists('mb_convert_encoding'))
+            $filnavn = mb_convert_encoding($filnavn, 'ISO-8859-1', 'UTF-8');
+        else
+        {
+            /** @noinspection PhpDeprecationInspection */
+            $filnavn = utf8_decode($filnavn);
+        }
+    }
 	return $filnavn;
 }
